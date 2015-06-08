d6fcd959d26e1c84bfefa92e68462684c88aaf37
psychomantys <psycho.mantys@gmail.com>
2015-06-08 01:22:04 -0300

    Gerando as fichas automaticamente...

--- make-pcgen-pdf.sh
+++ make-pcgen-pdf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./pcgen.sh -- -E outputsheets/d20/fantasy/pdf/csheet_fantasy_std_blackandwhite.xslt -c "$1"
+PCGEN_PATH="${HOME}/pcgen"
+
+PCGEN_TEMPLATES="${PCGEN_PATH}/outputsheets/d20/fantasy/pdf/csheet_fantasy_std_blackandwhite.xslt ${PCGEN_PATH}/outputsheets/d20/fantasy/htmlxml/csheet_fantasy_generic_export.xml.ftl"
+
+for t in $PCGEN_TEMPLATES ; do
+	${PCGEN_PATH}/pcgen.sh -- -E "${t}" -c "$(pwd)/$1"
+done
